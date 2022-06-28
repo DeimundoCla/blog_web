da6f793b01e6879da2a9a5ee986ac4342f5549e3
Deimundo <deimundocla@gmail.com>
2022-06-28 18:30:55 -0300

    backend0.4

--- blog_web/app_blog/models.py
+++ blog_web/app_blog/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils.text import slugify
 # Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+            
+    def get_absolute_url(self):
+        return reverse('index')
 
 class Posteo(models.Model):
     titulo = models.CharField(max_length=60)
@@ -10,13 +20,21 @@ class Posteo(models.Model):
     fecha_publicacion = models.DateTimeField(auto_now_add=True)
     contenido = models.TextField()
     imagen = models.ImageField(upload_to='blog/images', blank=True)
+    categoria = models.CharField(max_length=100, default='General')
+    url = models.SlugField(max_length=264, unique=True, null=True)
 
     def __str__(self):
         return self.titulo + ' | ' + str(self.autor)
 
+    def save(self, *args, **kwargs):
+        self.url = slugify(self.titulo)
+        super(Posteo, self).save(*args, **kwargs)
+
     def get_absolute_url(self):
-        return reverse('index')
-        
-        #("post", kwargs={"pk": self.pk})
+        return reverse("post", kwargs={"url": self.url})
+    
+
+    
+
     
 
--- blog_web/app_blog/views.py
+++ blog_web/app_blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Posteo
+from .models import Posteo, Categoria
 from .forms import PosteoForm
 from django.http import HttpResponse
 from django.template import loader
@@ -14,6 +14,13 @@ class Home(ListView):
 class Vistaposteo(DetailView):
     model = Posteo
     template_name = 'post.html'
+    slug_field = 'url'
+    slug_url_kwarg = 'url'
+
+class CargaCategoria(CreateView):
+    model = Categoria
+    template_name = 'carga_categoria.html'
+    fields = ['nome']
 
 class Cargapost(CreateView):
     model = Posteo
@@ -23,12 +30,16 @@ class Cargapost(CreateView):
 class Editarposteo(UpdateView):
     model = Posteo
     template_name = 'editar_post.html'
-    fields = ['titulo', 'subtitulo', 'contenido', 'imagen']
+    fields = ['titulo', 'subtitulo', 'contenido', 'imagen', 'categoria', 'url']
+    slug_field = 'url'
+    slug_url_kwarg = 'url'
 
 class Eliminarposteo(DeleteView):
     model = Posteo
     template_name = 'eliminar_post.html'
     success_url = '/'
+    slug_field = 'url'
+    slug_url_kwarg = 'url'
 
 
 def about(request):
@@ -40,3 +51,7 @@ def error(request):
     template = loader.get_template("404.html")
     documento = template.render()
     return HttpResponse(documento)
+
+def ViewCategoria(request, cate):
+    post_cats = Posteo.objects.filter(categoria = cate.replace('-', ' '))
+    return render(request, 'categorias.html', {'cate': cate.title().replace('-', ' '), 'post_cats': post_cats})
