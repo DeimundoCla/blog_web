38745a9aaf2f41f7c7ff68a4c3b1ef9b20f604c6
Alan Saja <alansaja13@gmail.com>
2022-06-27 22:19:37 -0300

    front0.3

--- blog_web/app_blog/urls.py
+++ blog_web/app_blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Editarposteo, Vistaposteo, Home, Cargapost, Eliminarposteo
+from .views import Editarposteo, Vistaposteo, Home, Cargapost, Eliminarposteo, about, error
 
 urlpatterns = [
     path('', Home.as_view(), name='index'),
@@ -7,5 +7,6 @@ urlpatterns = [
     path('carga_post/', Cargapost.as_view(), name='cargapost'),
     path('post/editar_post/<int:pk>', Editarposteo.as_view(), name='editarpost'),
     path('post/<int:pk>/borrar', Eliminarposteo.as_view(), name='eliminarpost'),
-
+    path('about', about, name="about" ),
+    path('404', error , name="404"),
 ]
